62bb65ed74170afbdc9aa32be5ea2bf9d98d283e
Ming Li <ming.george.li@gmail.com>
2017-05-01 23:21:29 +0100

    change image params

--- app/settings.py
+++ app/settings.py
@@ -2,11 +2,10 @@
 
 # Convolutional Neural Network
 MODEL_PATH = './trained_models/'
-IMAGE_PATH = './data/path_to_images/'
+IMAGE_PATH = './data/'
 # IMAGE_PATH = './data/original_images/'
-IMAGE_SHAPE = (90, 160, 3)
-# IMAGE_SHAPE = (720, 1280, 3)
-BATCH_SIZE = 64
+IMAGE_SHAPE = (128, 128, 3)
+BATCH_SIZE = 100
 MAX_STEPS = 1500
 ALPHA = 1e-3
 BETA = 1e-2
