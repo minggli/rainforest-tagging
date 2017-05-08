915c8f0bcdb088f45e7ffefbba0ff32230191e0f
Ming Li <ming.george.li@gmail.com>
2017-05-08 14:56:12 +0100

    adjust params

--- app/settings.py
+++ app/settings.py
@@ -6,8 +6,8 @@ import numpy as np
 MODEL_PATH = './trained_models/'
 IMAGE_PATH = './data/'
 # IMAGE_PATH = './data/original_images/'
-IMAGE_SHAPE = (128, 128, 3)
-BATCH_SIZE = 100
+IMAGE_SHAPE = (256, 256, 3)
+BATCH_SIZE = 128
 MAX_STEPS = 500
 ALPHA = 1e-3
 BETA = 1e-2
