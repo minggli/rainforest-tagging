9e1ce7d39b457a9f435af848b2711c262c305249
Ming Li <ming.george.li@gmail.com>
2017-05-19 15:38:08 +0100

    change metadata xgb

--- app/xgb/metadata.py
+++ app/xgb/metadata.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-metagb
+metadata
 
 gradient boosting trees on meta data of images (e.g. mean, kurtosis, skewness)
 """
@@ -22,11 +22,11 @@ def extract_meta_features(batch_tensor):
     """extract metadata information from expected 4-D image tensor
     (batch_size, width, height, dimension)"""
     # per image mean per channel resulting in (batch_size, dimension)
-    mean, variance = tf.nn.moments(batch_tensor, axis=(1, 2))
+    mean, variance = tf.nn.moments(batch_tensor, axes=(1, 2))
     std = tf.sqrt(variance)
     channel_max = tf.reduce_max(batch_tensor, axis=(1, 2))
     channel_min = tf.reduce_min(batch_tensor, axis=(1, 2))
-    mean_sub = batch_tensor - mean
+    mean_sub = batch_tensor - tf.reshape(mean, shape=[-1, 1, 1, 4])
     skewness = tf.reduce_mean(mean_sub ** 3, axis=(1, 2)) / std ** 3
     kurtosis = tf.reduce_mean(mean_sub ** 4, axis=(1, 2)) / std ** 4
     feature_stack = tf.stack(values=[mean, std, channel_max, channel_min,
@@ -37,19 +37,22 @@ def extract_meta_features(batch_tensor):
 
 
 @multithreading
-def materialise_data(meta_batch, label_batch):
+def materialise_data(meta_batch, label_batch, verbose=True):
     """simply aggregate data for Gradient Boosting Tree classifier"""
-    feats, labels = list(), list()
+    feats, labels, count = list(), list(), 0
     while True:
         try:
             meta, label = sess.run(fetches=[meta_batch, label_batch])
             feats.append(meta)
             labels.append(label)
+            count += 1
+            if verbose:
+                print('mini-batches loaded: {0}'.format(count))
         except tf.errors.OutOfRangeError as e:
             # pipe exhausted with pre-determined number of epochs i.e. 1
             break
         meta_flat = np.array([array for nested in feats for array in nested])
-        label_flat = np.array([array for nested in label for array in nested])
+        label_flat = np.array([array for nested in labels for array in nested])
     return meta_flat.reshape([-1, 6 * IMAGE_SHAPE[-1]]), label_flat
 
 
@@ -76,21 +79,29 @@ valid_image_batch, valid_label_batch = data_pipe(
                                                 shuffle=True)
 valid_meta_batch = extract_meta_features(valid_image_batch)
 
+init = tf.local_variables_initializer()
+
 sess = tf.Session()
-sess.run(tf.local_variables_initializer())
+sess.run(init)
+
+with sess:
+    X_valid, y_valid = materialise_data(valid_meta_batch, valid_label_batch)
+
+sess = tf.Session()
+sess.run(init)
 
 with sess:
     X_train, y_train = materialise_data(train_meta_batch, train_label_batch)
-    X_valid, y_valid = materialise_data(valid_meta_batch, valid_label_batch)
 
 # OvR for multi-label classification
-y_pred = np.zero_like(y_valid)
+y_pred = np.zeros_like(y_valid)
 models = list()
 for label_index in tqdm(range(17), miniters=1):
     clf = xgb.XGBClassifier(max_depth=5, learning_rate=0.1, n_estimators=100)
     clf.fit(X_train, y_train[:, label_index])
     # assign position probability to index location
     y_pred[:, label_index] = clf.predict_proba(X_valid)[:, 1]
+    print(y_pred[:, label_index][:5])
     models.append(clf)
 
 print('validation F2-score: {0}'.format(
@@ -117,9 +128,9 @@ with sess:
     X_test, y_test_sample = \
                 materialise_data(test_meta_batch, test_label_sample_batch)
 
-y_pred = np.zero_like(y_test_sample)
+y_pred = np.zeros_like(y_test_sample)
 for label_index in tqdm(range(17), miniters=1):
     clf = models[label_index]
     y_pred[:, label_index] = clf.predict_proba(X_test)[:, 1]
-
+    print(y_pred[:, label_index][:5])
 submit(y_pred, IMAGE_PATH + 'test', TAGS, TAGS_THRESHOLDS)
