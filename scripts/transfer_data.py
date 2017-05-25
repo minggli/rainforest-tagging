0782d0c88b070d4ba6752ad89b6cba05731c6768
Ming Li <ming.george.li@gmail.com>
2017-05-25 15:09:54 +0100

    multiprocess data transfer

--- scripts/transfer_data.py
+++ scripts/transfer_data.py
@@ -14,6 +14,7 @@ root = os.path.realpath('..')
 sys.path.append(root)
 
 from tqdm import tqdm
+from multiprocessing import Pool
 from app.pipeline import folder_traverse
 from app.settings import IMAGE_PATH, EXT, DEFAULT_BUCKET
 
@@ -25,32 +26,50 @@ ERASE = True if 'ERASE' in map(str.upper, sys.argv[1:]) else False
 BUCKETNAME = os.getenv(key='BUCKETNAME', default=DEFAULT_BUCKET)
 s3 = boto3.resource('s3')
 
+
+def upload_to_remote(local_path):
+    """upload data onto S3"""
+    remote_key_name = local_path.replace(root + '/./', '')
+    s3.meta.client.upload_file(Filename=local_path,
+                               Bucket=BUCKETNAME,
+                               Key=remote_key_name)
+
+
+def download_from_remote(remote_path):
+    """create local paths and download from Amazon S3"""
+    local_key_name = os.path.join(root, remote_path)
+    if not os.path.exists(os.path.dirname(local_key_name)):
+        try:
+            os.makedirs(os.path.dirname(local_key_name))
+        except FileExistsError:
+            pass
+    s3.meta.client.download_file(Key=remote_path,
+                                 Bucket=BUCKETNAME,
+                                 Filename=local_key_name)
+
+
 if UPLOAD:
+    p = Pool(8)
     fs = folder_traverse(os.path.join(root, IMAGE_PATH), ext=EXT)
     local_paths = [os.path.join(directory, filename)
                    for directory in fs for filename in fs[directory]]
+    for _ in tqdm(p.imap_unordered(upload_to_remote, local_paths),
+                  total=len(local_paths)):
+        pass
 
-    for local_path in tqdm(local_paths, miniters=1):
-        remote_key_name = local_path.replace(root + '/./', '')
-        s3.meta.client.upload_file(Filename=local_path,
-                                   Bucket=BUCKETNAME,
-                                   Key=remote_key_name)
 
 elif DOWNLOAD:
+    p = Pool(8)
     remote_paths = [obj.key for obj in s3.Bucket(BUCKETNAME).objects.all()]
-    for remote_path in tqdm(remote_paths, miniters=1):
-        local_key_name = os.path.join(root, remote_path)
-        if not os.path.exists(os.path.dirname(local_key_name)):
-            os.makedirs(os.path.dirname(local_key_name))
-        s3.meta.client.download_file(Key=remote_path,
-                                     Bucket=BUCKETNAME,
-                                     Filename=local_key_name)
+    for _ in tqdm(p.imap_unordered(download_from_remote, remote_paths),
+                  total=len(remote_paths)):
+        pass
 
-if ERASE:
+elif ERASE:
     confirm = input('Delete "{0}"? Type name of the bucket to proceed:'.format(
                     BUCKETNAME))
     if confirm != BUCKETNAME:
         print('Valid confirmation. No action taken.')
     elif confirm == BUCKETNAME:
         s3.Bucket(BUCKETNAME).objects.delete()
-        print('All objects in "{0}" deleted'.format(BUCKETNAME))
+        print('All objects in "{0}" deleted.'.format(BUCKETNAME))
