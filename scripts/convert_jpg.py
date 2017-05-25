f5de31edd8ea652bc8ac45e951997f9df95cf280
Ming Li <ming.george.li@gmail.com>
2017-05-25 11:56:48 +0100

    remove print statements

--- scripts/convert_jpg.py
+++ scripts/convert_jpg.py
@@ -8,23 +8,20 @@ convert CMYK colourspace to RGB
 
 import os
 import sys
-project_folder = os.path.realpath('..')
-sys.path.append(project_folder)
+root = os.path.realpath('..')
+sys.path.append(root)
 from PIL import Image
+from tqdm import tqdm
 
 from app.pipeline import folder_traverse
 from app.settings import IMAGE_PATH
 
-fs = folder_traverse(os.path.join(project_folder, IMAGE_PATH),
-                     ext=('.jpg'))
-path_to_images = list()
+fs = folder_traverse(os.path.join(root, IMAGE_PATH), ext=('.jpg'))
 
-for key in fs:
-    for filename in fs[key]:
-        path_to_images.append(os.path.join(key, filename))
+path_to_images = [os.path.join(key, filename) for key in fs
+                  for filename in fs[key]]
 
-for path_to_image in path_to_images:
+for path_to_image in tqdm(path_to_images, miniters=1):
     with Image.open(path_to_image) as img:
         if img.mode == 'CMYK':
-            print('converting from CMYK to RGB: {0}'.format(path_to_image))
             img.convert('RGB').save(path_to_image, 'JPEG')
