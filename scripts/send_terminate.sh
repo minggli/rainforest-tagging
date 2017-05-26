d34ead1bfecd3aae0d528baee5063dcb5a2d709a
Ming Li <ming.george.li@gmail.com>
2017-05-26 21:03:23 +0100

    include trained models

--- scripts/send_terminate.sh
+++ scripts/send_terminate.sh
@@ -4,6 +4,6 @@ export SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $SCRIPT_DIR
 
 aws s3 sync ../output s3://rainforest-save/output
-aws s3 sync ../trained_models s3://rainforest-save/models
+aws s3 sync ../trained_models s3://rainforest-save/trained_models
 
 sudo shutdown -h now
