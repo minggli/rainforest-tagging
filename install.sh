035e9b2f9ebf6a92fb82b01faa5c60e8663b81d1
Ming Li <ming.george.li@gmail.com>
2017-05-26 19:23:11 +0100

    fix install.sh

--- install.sh
+++ install.sh
@@ -2,23 +2,24 @@
 
 pip3 install virtualenv
 virtualenv amazon -p python3
-amazon/bin/pip3 install --upgrade pip
-amazon/bin/pip3 install -r requirements.txt
+source amazon/bin/activate
+
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
 
 # so xgboost can be installed via pip on ubuntu but strangely not on Mac
 # assuming you have python setup tools and C++ compiler either gcc or clang
 if [ "$(uname)" == "Darwin" ]; then
 git clone --recursive https://github.com/dmlc/xgboost
 cd xgboost; cp make/minimum.mk ./config.mk; make -j4
-source amazon/bin/activate
 cd python-package; sudo python3 setup.py install
 cd ..; cd ..
 
 rm -rf xgboost
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-amazon/bin/pip3 install xgboost
+pip3 install xgboost
 fi
 
 # glove vectors and fuller vocabulary
-amazon/bin/python3 -m spacy download en_vectors_glove_md
-amazon/bin/python3 -m spacy download en_core_web_md
+python3 -m spacy download en_vectors_glove_md
+python3 -m spacy download en_core_web_md
