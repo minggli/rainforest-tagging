d3be906daf3c6d24fcb24e15311dd6d0670b0720
Ming Li <ming.george.li@gmail.com>
2017-06-27 16:05:42 +0100

    increase threads to higher gpu utilisation

--- app/settings.py
+++ app/settings.py
@@ -8,7 +8,7 @@ DEFAULT_BUCKET = 'rainforest-satellite-images'
 EXT = ('.png', '.csv')
 IMAGE_SHAPE = (64, 64, 4)
 BATCH_SIZE = 64
-N_THREADS = 2
+N_THREADS = 4
 # roughly 10 epochs of training data
 MAX_STEPS = 5000
 EPOCHS = 5
