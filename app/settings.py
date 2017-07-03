f139e19462608cc87141b335592a2b64a37ff0f5
Ming Li <ming.george.li@gmail.com>
2017-07-03 11:39:45 +0100

    adjust param

--- app/settings.py
+++ app/settings.py
@@ -6,12 +6,12 @@ IMAGE_PATH = './data/'
 OUTPUT_PATH = './output/'
 DEFAULT_BUCKET = 'rainforest-satellite-images'
 EXT = ('.jpg', '.csv')
-IMAGE_SHAPE = (128, 128, 3)
+IMAGE_SHAPE = (64, 64, 3)
 BATCH_SIZE = 96
 N_THREADS = 4
 # roughly 10 epochs of training data
 MAX_STEPS = 5000
-EPOCHS = 5
+EPOCHS = 3
 
 ALPHA = 5e-4
 BETA = 1e-2
